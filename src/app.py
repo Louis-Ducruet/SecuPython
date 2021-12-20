2ab473f16735bca162cdb779aa6f1d18a3c1f204
Louis-Ducruet <louis.ducruet@epsi.fr>
2021-12-20 11:46:30 +0100

    add function in app.py

--- src/app.py
+++ src/app.py
@@ -4,11 +4,19 @@ import os
 
 def afficheMenu(affichage: affichage.Affichage):
     menu = [
-        "chiffrer (.txt)", "déchiffrer (.txt)",
-        "chiffrer (.jpg, .png)", "déchiffrer (.jpg, .png)",
-        "les paramètres", "se déconnecter"
+        "chiffrer (.txt)", "déchiffrer (.txt)", "chiffrer (.jpg, .png)", "déchiffrer (.jpg, .png)", "les paramètres",
+        "se déconnecter"
     ]
     print(affichage.info("Menu SecuPython"))
+    print(affichage.menu(menu))
+
+
+def afficheParametre(affichage: affichage.Affichage):
+    menu = [
+        "changer d'adresse mail", "changer de login", "changer de mot de passe", "ajouter un utilisateur",
+        "supprimer ce compte", "retour au menu principale"
+    ]
+    print(affichage.info("Paramètres SecuPython"))
     print(affichage.menu(menu))
 
 
