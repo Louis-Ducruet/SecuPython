a2c4b53e293a35875c705b221142a7e4137cc3e8
Louis-Ducruet <louis.ducruet@epsi.fr>
2021-12-21 14:32:48 +0100

    fix order load error

--- main.py
+++ main.py
@@ -1,25 +1,25 @@
 import src.actionMenu as actionMenu
 import src.app as app
-import src.connexion as connexion
 import src.classe.affichage as affichage
 import src.classe.email as email
 import src.classe.fichier as fichier
 import src.classe.securite as securite
 import src.classe.user as user
+import src.connexion as connexion
 import src.verification as verification
 
-chiffFile = securite.Securite([[13, 24], [8, 14]], [[-7 / 5, 12 / 5], [4 / 5, -13 / 10]])
-chiffSys = securite.Securite([[2, 5], [1, 3]], [[3, -5], [-1, 2]])
-dossier = fichier.Fichier("input", "output")
 terminal = affichage.Affichage()
-utilisateur = user.User(dossier, chiffSys)
-messagerie = email.Email(utilisateur)
 
 
 def start():
     app.efface(terminal)
+    dossier = fichier.Fichier("input", "output")
+    chiffSys = securite.Securite([[2, 5], [1, 3]], [[3, -5], [-1, 2]])
     if not verification.verification(dossier, terminal, chiffSys):
         return False
+    chiffFile = securite.Securite([[13, 24], [8, 14]], [[-7 / 5, 12 / 5], [4 / 5, -13 / 10]])
+    utilisateur = user.User(dossier, chiffSys)
+    messagerie = email.Email(utilisateur)
     if not connexion.connexion(dossier, terminal, chiffSys, utilisateur, messagerie):
         return False
     while True:
